2cee9720d3a1a76b97130db5785849b0461a005f
knguyen22 <knguyen22@hawk.iit.edu>
2024-01-10 22:58:22 -0600

    add initial GenAI integration

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 import datetime
 import json
 import random 
+import torch
+
+from transformers import pipeline
+
 
 def date_to_puzzle(date):
     first_puzzle_id = 204
@@ -25,7 +29,33 @@ def load_words(puzzle_content):
     return answers, words
 
 def llm_suggestion(words):
-    pass
+    pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v0.6", torch_dtype=torch.bfloat16)
+
+    word_lst = ', '.join(words)
+
+    messages = [
+        {
+            "role": "system",
+            "content": '''
+                You are a human trying to solve a puzzle using your knowledge on different word meanings, slangs, idioms, and popular culture references
+            ''',
+        },
+        {
+            "role": "user", 
+            "content": f'''
+                Given the list of words: {word_lst}
+                
+                Solve the puzzle by following these exact rules:
+                - Each word in the list of words above belongs to exactly 1 unspecified category. 
+                - There are 4 categories in total with each category containing 4 different words
+                - Determine the 4 categories and the 4 respective words belonging to them
+
+            '''},
+    ]
+    prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+    res = outputs[0]["generated_text"]
+    return res.split('<|assistant|>', 1)[1].lower()
 
 if __name__ == '__main__':
     date_str = input('Choose date (MM/DD/2024): ')
@@ -46,6 +76,9 @@ if __name__ == '__main__':
     for i in range(4):
         print('| ' + ' | '.join(words[i*2: i*2 + 4]) + ' |')
 
+    print('\n----- GenAI Suggestion -----')
+    print(llm_suggestion(words))
+    
     i = 0
     while i < 4:
         reveal = input('\n reveal 1 group (y/n)?: ')
@@ -55,5 +88,5 @@ if __name__ == '__main__':
             print(f'{group}: '+ ', '.join(answers[group]))
             i += 1
         else:
-            continue
+            break
         
